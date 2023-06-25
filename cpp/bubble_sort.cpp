39fdaac67f24f5a8d5c99d8075179bc3225772d1
Gleb <grg0410@mail.ru>
2023-06-25 23:22:39 +0300

    bubble_sort cpp

---
+++ cpp/bubble_sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+template<std::size_t Size>
+void bubble_sort(int arr[]) {
+    while (true) {
+        bool changes = false;
+        for (int ind = 0; ind < Size - 1; ind++) {
+            if (arr[ind] > arr[ind + 1]) {
+                std::swap(arr[ind], arr[ind + 1]);
+                changes = true;
+            }
+        }
+        if (!changes) break;
+    } // no need to return the array
+}
+
+int main() {
+    int example[] = {5, 1, 6, 3, 4};
+    bubble_sort <5> (example);
+    for (int ind = 0; ind < sizeof(example) / 4; ind++) {
+        std::cout << example[ind] << ' ';
+    }
+    return 0;
+}
