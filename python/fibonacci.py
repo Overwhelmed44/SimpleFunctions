3a94987f672e015d73a1e6853a9dd62dcf5a3593
Gleb <grg0410@mail.ru>
2023-06-26 00:16:56 +0300

    fibonacci python

---
+++ python/fibonacci.py
@@ -0,0 +1,9 @@
+def main():
+    n = int(input())
+    print(fibonacci(n))
+
+def fibonacci(n, first=0, second=1):
+    return fibonacci(n-1, second, first + second) if n else first
+
+if __name__ == '__main__':
+    main()
