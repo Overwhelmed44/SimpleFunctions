c8a7e2b47b026df1c796f47f177b564a5dab72a7
Gleb <grg0410@mail.ru>
2023-06-26 20:30:29 +0300

    shaker_sort cpp

---
+++ cpp/shaker_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+template<std::size_t Size>
+void shaker_sort(int arr[]) {
+    bool changes = true;
+    while (changes) {
+        changes = false;
+        for (int ind = 0; ind < Size - 1; ind++) {
+            if (arr[ind] > arr[ind + 1]) {
+                std::swap(arr[ind], arr[ind + 1]);
+                changes = true;
+            }
+        }
+        if (!changes) break;
+        changes = false;
+        for (int ind = Size - 1; ind > 0; ind--) {
+            if (arr[ind] < arr[ind - 1]) {
+                std::swap(arr[ind], arr[ind - 1]);
+                changes = true;
+            }
+        }
+    } // no need to return the array
+}
+
+int main() {
+    int example[] = {5, 1, 6, 3, 4};
+    shaker_sort <5> (example);
+    for (int ind = 0; ind < sizeof(example) / 4; ind++) {
+        std::cout << example[ind] << ' ';
+    }
+    return 0;
+}
