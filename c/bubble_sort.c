aba9f07ec6c925e461cf4e40c9aacfc2987df2d1
Gleb <grg0410@mail.ru>
2023-06-26 15:12:11 +0300

    bubble_sort c

---
+++ c/bubble_sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int length) {
+    length--;
+    while (1) {
+        int changes = 0;
+        for (int ind = 0; ind < length; ind++) {
+            if (arr[ind] > arr[ind + 1]) {
+                int swap = arr[ind];
+                arr[ind] = arr[ind + 1];
+                arr[ind + 1] = swap;
+                changes = 1;
+            }
+        }
+        if (changes == 0) break;
+    } // no need to return the array
+}
+
+int main() {
+    int example[] = {5, 1, 6, 3, 4};
+    int length = sizeof(example) / 4;
+    bubble_sort(example, length);
+    for (int ind = 0; ind < length; ind++) {
+        printf("%d ", example[ind]);
+    }
+    return 0;
+}
