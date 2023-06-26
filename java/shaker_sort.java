f42a4f854ad267bc934721bb1f8514bdd0e8aebc
Gleb <grg0410@mail.ru>
2023-06-26 21:03:05 +0300

    shaker_sort java

---
+++ java/shaker_sort.java
@@ -0,0 +1,36 @@
+public class shaker_sort {
+    public static void main(String[] args) {
+        int[] example = {5, 1, 6, 3, 4};
+        shaker_sort_(example);
+        for (int ind = 0; ind < example.length; ind++) {
+            System.out.printf("%d ", example[ind]);
+        }
+    }
+
+    public static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    public static void shaker_sort_(int[] arr) {
+        int l = arr.length - 1;
+        boolean changes = true;
+        while (changes) {
+            changes = false;
+            for (int ind = 0; ind < l; ind++) {
+                if (arr[ind] > arr[ind + 1]) {
+                    swap(arr, ind, ind + 1);
+                    changes = true;
+                }
+            }
+            if (!changes) break;
+            for (int ind = l; ind > 0; ind--) {
+                if (arr[ind] < arr[ind - 1]) {
+                    swap(arr, ind, ind - 1);
+                    changes = true;
+                }
+            }
+        } // no need to return the array
+    }
+}
